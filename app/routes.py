1eb34fa9e9fc100aeec55ad24e19d46227f16117
Thanh Nguyen <nguyenhm.thanh@gmail.com>
2019-10-06 17:49:00 -0700

    add database for user account

--- app/routes.py
+++ app/routes.py
@@ -1,27 +1,46 @@
 from flask import render_template, url_for, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
-from app import app
+from app import app, db
 from app.forms import LoginForm, SignupForm
+from flask_login import login_user, logout_user, current_user, login_required
+from app.models import User
+from werkzeug.urls import url_parse
 
 
 @app.route('/', methods=['POST', 'GET'])
+@app.route('/index', methods=['POST', 'GET'])
 def index():
         return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html',  title='Log In', form=form)
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = SignupForm()
     if form.validate_on_submit():
-        flash('Signup requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
     return render_template('signup.html',  title='Sign Up', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
