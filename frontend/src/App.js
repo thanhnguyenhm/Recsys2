c6cc5aa359124bea9e530569892a6ab113b1b071
Thanh Nguyen <nguyenhm.thanh@gmail.com>
2019-10-25 01:06:47 -0700

    update React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import "./App.css";
-import Movies from "./components/Movies";
 import NavBar2 from './components/NavBar2'
 import Login from './components/Login'
 import Register from './components/Register'
 import Profile from './components/Profile'
+import Popular from './components/Popular'
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -18,20 +18,17 @@ function App() {
     );
   }, []);
 
-  console.log({movies})
+  console.log({ movies })
 
   return (
     <Router>
       <div className="App" >
         <NavBar2 />
         <div className="container">
+          <Route exact path="/" component={Popular} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/profile" component={Profile} />
-        </div>
-        <div className='.f2 tc'>
-          <h1 id='title'>Popular Movies</h1>
-          <Movies movies={movies} />
         </div>
       </div>
     </Router>
---
+++ frontend/src/components/Popular.js
@@ -0,0 +1,23 @@
+import Movies from "./Movies";
+import React, { useEffect, useState } from "react";
+
+function Popular() {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch("/index").then(response =>
+            response.json().then(data => {
+                setMovies(data);
+            })
+        );
+    }, []);
+
+    return (
+        <div className='.f2 tc'>
+            <h1 id='title'>Popular Movies</h1>
+            <Movies movies={movies} />
+        </div>
+    )  
+}
+
+export default Popular;
