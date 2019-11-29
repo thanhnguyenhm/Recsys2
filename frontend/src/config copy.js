da50ff0adabe2f65e0291c3c3d386011a3fc51a9
Thanh Nguyen <nguyenhm.thanh@gmail.com>
2019-11-28 20:21:19 -0800

    change setting for production

---
+++ frontend/src/config copy.js
@@ -0,0 +1,11 @@
+// Configuration for TMDB
+// To se the latest configuration fetch it from https://api.themoviedb.org/3/configuration?api_key=019e8f375549e0bbd4a4191862ebc88f
+
+const API_URL = 'https://api.themoviedb.org/3/movie/';
+const API_KEY = 'REPLACE YOUR API KEY HERE AND CHANGE FILE NAME TO config.js';
+
+
+export {
+  API_URL,
+  API_KEY,
+}
