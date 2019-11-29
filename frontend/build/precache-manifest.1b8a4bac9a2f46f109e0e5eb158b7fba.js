6f397033c9806527b9ef8735958fa7e3a04e4374
Thanh Nguyen <nguyenhm.thanh@gmail.com>
2019-11-29 01:48:36 -0800

    prepare github pages

---
+++ frontend/build/precache-manifest.1b8a4bac9a2f46f109e0e5eb158b7fba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "712b8698f4362a422ee09340a061f00e",
+    "url": "/thanhnguyenhm/Recsys2/index.html"
+  },
+  {
+    "revision": "d56f4fe80080982f7b35",
+    "url": "/thanhnguyenhm/Recsys2/static/css/2.9d6dfeae.chunk.css"
+  },
+  {
+    "revision": "96caaaa15e753caa267f",
+    "url": "/thanhnguyenhm/Recsys2/static/css/main.75827616.chunk.css"
+  },
+  {
+    "revision": "d56f4fe80080982f7b35",
+    "url": "/thanhnguyenhm/Recsys2/static/js/2.49e35eb3.chunk.js"
+  },
+  {
+    "revision": "96caaaa15e753caa267f",
+    "url": "/thanhnguyenhm/Recsys2/static/js/main.e23e99e8.chunk.js"
+  },
+  {
+    "revision": "a6a0ed09af01dddd4b3e",
+    "url": "/thanhnguyenhm/Recsys2/static/js/runtime-main.e38d5b57.js"
+  },
+  {
+    "revision": "ddfab15459f4eb0b1dfb4319529839b4",
+    "url": "/thanhnguyenhm/Recsys2/static/media/no_image.ddfab154.png"
+  }
+]);
